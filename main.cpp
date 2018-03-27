8182f622433f49edd1b3e50c5700d67741858ceb
svang56 <svang56@mail.csuchico.edu>
2018-03-26 17:53:05 -0700

    done

--- main.cpp
+++ main.cpp
@@ -69,7 +69,7 @@ int main()
     for(int r = 0; r < 3; r++)
     {
         for(int c = 0; c < 3; c++)
-        {cout <<board[r][c];
+        {cout <<board[r][c]<<" ";
         }
         cout<<endl;
         }
